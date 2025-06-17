6b385c76d8a9ef5762c8fdaa66f9f2a934963128
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-17 17:30:42 -0400

    feat(table): edit model so the email is the same as the username

--- transformations/models/mock/mock_users.sql
+++ transformations/models/mock/mock_users.sql
@@ -3,14 +3,13 @@
 WITH raw_data AS (
     SELECT 
         generate_series(1, 100) AS user_id,
-        md5(random()::text) AS hash,
         now() - (random() * interval '365 days') AS created_at,
         CASE WHEN random() > 0.5 THEN 'active' ELSE 'inactive' END AS status
 )
 SELECT 
     user_id,
     CONCAT('user_', user_id) AS username,
-    CONCAT(hash, '@example.com') AS email,
+    CONCAT('user_', user_id, '@email.com') AS email,
     created_at,
     status
 FROM raw_data
