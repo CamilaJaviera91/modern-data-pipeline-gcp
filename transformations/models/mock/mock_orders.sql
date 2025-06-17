7c70976558df86a79fd6b50070c07729ff4e4f1a
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-17 17:47:37 -0400

    feat(table): edit field

--- transformations/models/mock/mock_orders.sql
+++ transformations/models/mock/mock_orders.sql
@@ -5,7 +5,7 @@ WITH order_data AS (
         generate_series(1, 200) AS order_id,
         floor(random() * 100 + 1)::int AS user_id,  -- IDs entre 1 y 100
         now() - (random() * interval '180 days') AS order_date,
-        round(random() * 500 + 10, 2) AS amount,
+        round((random() * 500 + 10)::numeric, 2) AS amount, AS amount,
         CASE 
             WHEN random() > 0.8 THEN 'cancelled'
             WHEN random() > 0.5 THEN 'shipped'
