787e065e811b50dd6660494c6463f6a4ca288e00
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-19 17:43:01 -0400

    feat(sql): edit table so we can create real names

--- transformations/models/mock/mock_users.sql
+++ transformations/models/mock/mock_users.sql
@@ -7,27 +7,45 @@
 
 -- models/mock/mock_users.sql
 
-WITH base AS (
+WITH first_names AS (
+    SELECT *
+    FROM unnest(ARRAY[
+        'Camila', 'Luis', 'Javiera', 'Pedro', 'Ignacia', 'Felipe', 'Sofía', 'José', 'Valentina', 'Andrés', 'Javier'
+    ]) WITH ORDINALITY AS t(first_name, id)
+),
+last_names AS (
+    SELECT *
+    FROM unnest(ARRAY[
+        'Muñoz', 'Pérez', 'González', 'Rojas', 'Contreras', 'Díaz', 'Morales', 'Castro', 'Torres', 'Silva', 'Acevedo'
+    ]) WITH ORDINALITY AS t(last_name, id)
+),
+base AS (
     SELECT 
-    generate_series(1, 1000) AS user_id,
-    (random() * interval '365 days') AS random_date,
-    CASE 
-        WHEN random() > 0.5 THEN 'active'
-        ELSE 'inactive'
-    END AS status
+        generate_series(1, 1000) AS user_id,
+        (random() * interval '365 days') AS random_date,
+        floor(random() * 10 + 1)::int AS fn_id,
+        floor(random() * 10 + 1)::int AS ln_id,
+        CASE 
+            WHEN random() > 0.5 THEN 'active'
+            ELSE 'inactive'
+        END AS status
 ),
 user_data AS (
     SELECT
-        user_id,
-        CONCAT('User_', user_id) AS username,
-        CONCAT('User_', user_id, '@example.com') AS email,
-        now() - random_date AS created_at,
-        status,
+        b.user_id,
+        fn.first_name,
+        ln.last_name,
+        CONCAT(fn.first_name, '_', ln.last_name, '_', b.user_id) AS username,
+        CONCAT(fn.first_name, '.', ln.last_name, b.user_id, '@example.com') AS email,
+        now() - b.random_date AS created_at,
+        b.status,
         CASE
-            WHEN status = 'active' THEN NULL
-            ELSE random_date + now()
+            WHEN b.status = 'active' THEN NULL
+            ELSE b.random_date + now()
         END AS terminated_at
-    FROM base
+    FROM base b
+    LEFT JOIN first_names fn ON b.fn_id = fn.id
+    LEFT JOIN last_names ln ON b.ln_id = ln.id
 )
 
 SELECT * FROM user_data
