cabd9f2dcdd993cce3e98a75a2ca96232e9b15e4
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-19 16:14:10 -0400

    feat(sql): edit table

--- transformations/models/mock/mock_users.sql
+++ transformations/models/mock/mock_users.sql
@@ -8,15 +8,19 @@
 -- models/mock/mock_users.sql
 
 WITH base AS (
+    SELECT generate_series(1, 1000) AS user_id
+),
+user_data AS (
     SELECT
-    generate_series(1, 1000) AS user_id,
-    ),
-    user_data AS (
-        SELECT
         user_id,
-        CONCAT("User_",user_id) AS username,
-        CONCAT("User_",user_id'@example.com') AS email,
+        CONCAT('User_', user_id) AS username,
+        CONCAT('User_', user_id, '@example.com') AS email,
         now() - (random() * interval '365 days') AS created_at,
-        CASE WHEN random() > 0.5 THEN 'active' ELSE 'inactive' END AS status
+        CASE
+            WHEN random() > 0.5 THEN 'active'
+            ELSE 'inactive'
+        END AS status
+    FROM base
 )
-SELECT * FROM user_data
+
+SELECT * FROM user_data;
