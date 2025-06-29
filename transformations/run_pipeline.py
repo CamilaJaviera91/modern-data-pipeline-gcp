192fad4aac0234b92496069ad532dfc20ad5e923
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-29 16:49:17 -0400

    feat(pipeline): update run_pipeline with the rates

--- transformations/run_pipeline.py
+++ transformations/run_pipeline.py
@@ -5,10 +5,12 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 from scripts.export_tables import export_all_tables
+from scripts.load_exchange_rates import run_exchange_rates_load
 
 # Call when needed in your DAG or script
 if __name__ == "__main__":
     
     print("🚀 Starting pipeline...")
     export_all_tables()
+    run_exchange_rates_load()
     print("✅ Pipeline completed.")
