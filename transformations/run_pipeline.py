9112fd5b6bc0eaa7e98861c0bc66b05ad296e4b9
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-29 17:03:36 -0400

    feat(pipeline): add upload and change names

--- transformations/run_pipeline.py
+++ transformations/run_pipeline.py
@@ -4,13 +4,15 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
-from scripts.export_tables import export_all_tables
-from scripts.load_exchange_rates import run_exchange_rates_load
+from scripts.upload_tables import run_dbt_mock_models as upload
+from scripts.export_tables import export_all_tables as export
+from scripts.load_exchange_rates import run_exchange_rates_load as rates
 
 # Call when needed in your DAG or script
 if __name__ == "__main__":
     
     print("🚀 Starting pipeline...")
-    export_all_tables()
-    run_exchange_rates_load()
+    upload()
+    export()
+    rates()
     print("✅ Pipeline completed.")
