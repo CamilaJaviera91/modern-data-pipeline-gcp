bca781d458df3802c6b889d2ddd6071c982fcf00
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-29 16:23:38 -0400

    feat(pipeline): add export_tables into the pipeline

--- transformations/scripts/run_pipeline.py
+++ transformations/scripts/run_pipeline.py
@@ -2,7 +2,7 @@
 from scripts.export_tables import export_all_tables
 
 # Call when needed in your DAG or script
-if __name__ = "__main__":
+if __name__ == "__main__":
     
     print("🚀 Starting pipeline...")
     export_all_tables()
