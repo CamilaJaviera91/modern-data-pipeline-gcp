f363467f217c5bea8e9da335e3cf9e2204a61067
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-30 15:24:25 -0400

    feat(pipeline): add export to csv

--- transformations/run_pipeline.py
+++ transformations/run_pipeline.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".
 
 from scripts.load_exchange_rates import run_exchange_rates_load as rates
 from scripts.upload_tables import run_dbt_mock_models as upload
+from scripts.export_csv import export_tables_to_csv as csv
 
 # Call when needed in your DAG or script
 if __name__ == "__main__":
@@ -13,4 +14,5 @@ if __name__ == "__main__":
     print("🚀 Starting pipeline...")
     rates()
     upload()
+    csv()
     print("✅ Pipeline completed.")
