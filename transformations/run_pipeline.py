2c63f474ab32af130573b54ee2e18baf121fb1c1
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-06-30 15:23:22 -0400

    feat(pipeline): change order so we create first the rates

--- transformations/run_pipeline.py
+++ transformations/run_pipeline.py
@@ -4,15 +4,13 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
-from scripts.upload_tables import run_dbt_mock_models as upload
-from scripts.export_tables import export_all_tables as export
 from scripts.load_exchange_rates import run_exchange_rates_load as rates
+from scripts.upload_tables import run_dbt_mock_models as upload
 
 # Call when needed in your DAG or script
 if __name__ == "__main__":
     
     print("🚀 Starting pipeline...")
-    upload()
-    export()
     rates()
+    upload()
     print("✅ Pipeline completed.")
