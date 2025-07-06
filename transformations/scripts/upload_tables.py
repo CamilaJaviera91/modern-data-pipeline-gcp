2832719668632dbb60ba32e7d9ed2a4ec6796253
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-07-06 19:30:38 -0400

    feat(rundbt): change process

--- transformations/scripts/upload_tables.py
+++ transformations/scripts/upload_tables.py
@@ -3,7 +3,7 @@ import subprocess
 def run_dbt_mock_models():
     print("🚀 Running DBT models: mock")
     try:
-        subprocess.run(["./run.sh", "run", "--select", "mock"], check=True)
+        subprocess.run(["dbt", "run", "--select", "mock"], check=True)
         print("✅ DBT models ran successfully")
     except subprocess.CalledProcessError as e:
         print(f"❌ DBT command failed with exit code {e.returncode}")
